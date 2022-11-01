892e0c4c02f27f8e3d6291d362753c917a53136f
Vab31 <97352743+Vab31@users.noreply.github.com>
2022-11-01 23:54:58 +0530

    ch

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  weather_base_url:'https://visual-crossing-weather.p.rapidapi.com/forecast?',
+  XRapidAPIKey_header:'X-RapidAPI-Key',
+  XRapidAPIKey_value:'84bbf6a386msh5fe52b52dda5810p1872cbjsnd6d0baf22d43',
+
+  XRapidAPIHost_header:"X-RapidAPI-Host",
+  XRapidAPIHost_value:"visual-crossing-weather.p.rapidapi.com",
+};
